7887142ebdac73259ab7cbcc8cbe74cd8151fa73
JWTK <jonaszwitek@gmail.com>
2025-11-01 18:13:42 +0100

    updates

--- src/OAuth/Client/Program.cs
+++ src/OAuth/Client/Program.cs
@@ -1,11 +1,14 @@
-namespace OAuth;
+namespace Client;
 
 public class Program
 {
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+
         var app = builder.Build();
+
+
         app.Run();
     }
 }
