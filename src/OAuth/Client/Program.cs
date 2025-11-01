ea86248942d98ce41251b51ba1cb7c6eb6b09d0a
JWTK <jonaszwitek@gmail.com>
2025-11-01 20:33:24 +0100

    updates

--- src/OAuth/Client/Program.cs
+++ src/OAuth/Client/Program.cs
@@ -1,3 +1,6 @@
+using System.Text.Json;
+using Microsoft.AspNetCore.Authentication;
+
 namespace Client;
 
 public class Program
@@ -6,8 +9,49 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
+        builder.Services.AddAuthentication("cookie")
+            .AddCookie("cookie")
+            .AddOAuth("custom", o =>
+            {
+                o.SignInScheme = "cookie";
+
+                o.ClientId = "x";
+                o.ClientSecret = "x";
+
+                o.AuthorizationEndpoint = "https://localhost:5005/oauth/authorize";
+                o.TokenEndpoint = "https://localhost:5005/oauth/token";
+                o.CallbackPath = "/oauth/custom-cb";
+
+                o.UsePkce = true;
+                o.ClaimActions.MapJsonKey("sub", "sub");
+                o.ClaimActions.MapJsonKey("custom 33", "custom");
+        
+                o.Events.OnCreatingTicket = ctx =>
+                {
+                    var payloadBase64 = ctx.AccessToken?.Split('.')[1];
+                    var payloadJson = Base64UrlTextEncoder.Decode(payloadBase64!);
+                    var payload = JsonDocument.Parse(payloadJson);
+                    ctx.RunClaimActions(payload.RootElement);
+                    return Task.CompletedTask;
+                };
+            });
+
         var app = builder.Build();
 
+        app.MapGet("/", (
+            HttpContext ctx
+        ) =>
+        {
+            return ctx.User.Claims.Select(x => new { x.Type, x.Value }).ToList();
+        });
+
+        app.MapGet("/login", () => Results.Challenge(
+            new AuthenticationProperties()
+            {
+                RedirectUri = "https://localhost:5004/"
+            },
+            authenticationSchemes: new List<string>() { "custom" }
+        ));
 
         app.Run();
     }
--- src/OAuth/OAuthServer/Program.cs
+++ src/OAuth/OAuthServer/Program.cs
@@ -1,3 +1,7 @@
+using OAuthServer;
+using OAuthServer.Endpoints;
+using OAuthServer.Endpoints.OAuth;
+
 namespace OAuthServer;
 
 public class Program
@@ -5,7 +9,22 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddAuthentication("cookie")
+            .AddCookie("cookie", o =>
+            {
+                o.LoginPath = "/login";
+            });
+        builder.Services.AddAuthorization();
+        builder.Services.AddSingleton<DevKeys>();
+
         var app = builder.Build();
+
+        app.MapGet("/login", GetLogin.Handler);
+        app.MapPost("/login", Login.Handler);
+        app.MapGet("/oauth/authorize", AuthorizationEndpoint.Handle).RequireAuthorization();
+        app.MapPost("/oauth/token", TokenEndpoint.Handle);
+
         app.Run();
     }
 }
