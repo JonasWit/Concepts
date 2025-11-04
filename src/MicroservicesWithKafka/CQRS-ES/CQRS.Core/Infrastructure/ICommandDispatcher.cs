d5b2bbd1df0b7488c1c12955bf6e70da8647f9c0
JWTK <jonaszwitek@gmail.com>
2025-11-04 18:08:40 +0100

    updates

---
+++ src/MicroservicesWithKafka/CQRS-ES/CQRS.Core/Infrastructure/ICommandDispatcher.cs
@@ -0,0 +1,11 @@
+using CQRS.Core.Commnds;
+
+namespace CQRS.Core.Infrastructure
+{
+
+    public interface ICommandDispatcher
+    {
+        void RegisterHandler<T>(Func<T, Task> handler) where T : BaseCommand;
+        Task SendAsync(BaseCommand command);
+    }
+}
