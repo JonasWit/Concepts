f932a71160224662acc83e83c9569fdb75a9e8b7
JWTK <jonaszwitek@gmail.com>
2025-11-06 13:55:23 +0100

    updates

--- src/MicroservicesWithKafka/SM-Post/Post.Cmd/Post.Cmd.Api/Program.cs
+++ src/MicroservicesWithKafka/SM-Post/Post.Cmd/Post.Cmd.Api/Program.cs
@@ -1,3 +1,4 @@
+using Confluent.Kafka;
 using CQRS.Core.Commnds;
 using CQRS.Core.Domain;
 using CQRS.Core.Handlers;
@@ -13,6 +14,7 @@ using Post.Cmd.Infrastructure.Stores;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.Configure<MongoDbConfig>(builder.Configuration.GetSection(nameof(MongoDbConfig)));
+builder.Services.Configure<ProducerConfig>(builder.Configuration.GetSection(nameof(ProducerConfig)));
 builder.Services.AddScoped<IEventStoreRepository, EventStoreRepository>();
 builder.Services.AddScoped<IEventStore, EventStore>();
 builder.Services.AddScoped<IEventSourcingHandler<PostAggregate>, EventSourcingHandler>();
--- src/MicroservicesWithKafka/SM-Post/Post.Cmd/Post.Cmd.Infrastructure/Producers/EventProducer.cs
+++ src/MicroservicesWithKafka/SM-Post/Post.Cmd/Post.Cmd.Infrastructure/Producers/EventProducer.cs
@@ -1,6 +1,32 @@
+using System.Text.Json;
+using Confluent.Kafka;
+using CQRS.Core.Events;
+using CQRS.Core.Producers;
+using Microsoft.Extensions.Options;
+
 namespace Post.Cmd.Infrastructure.Producers;
 
-public class EventProducer
+public class EventProducer(IOptions<ProducerConfig> producerConfig) : IEventProducer
 {
-    
+    private readonly ProducerConfig _producerConfig = producerConfig.Value;
+
+    public async Task ProduceAsync<T>(string topic, T @event) where T : BaseEvent
+    {
+        using var producer = new ProducerBuilder<string, string>(_producerConfig)
+            .SetKeySerializer(Serializers.Utf8)
+            .SetValueSerializer(Serializers.Utf8)
+            .Build();
+
+        var eventMessage = new Message<string, string>()
+        {
+            Key = Guid.NewGuid().ToString(),
+            Value = JsonSerializer.Serialize(@event, @event.GetType())
+        };
+        
+        var deliveryResult = await producer.ProduceAsync(topic, eventMessage);
+        if (deliveryResult.Status == PersistenceStatus.NotPersisted)
+        {
+            throw new Exception($"Could not produce {@event.GetType().Name} event to topic {topic}, reason: {deliveryResult.Message!}");
+        }
+    }
 }
