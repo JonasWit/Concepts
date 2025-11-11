e74f39a41bc4cb504113c138776001896b51ae63
JWTK <jonaszwitek@gmail.com>
2025-11-11 18:25:50 +0100

    60

--- src/MicroservicesWithKafka/SM-Post/Post.Cmd/Post.Cmd.Api/Controllers/NewPostController.cs
+++ src/MicroservicesWithKafka/SM-Post/Post.Cmd/Post.Cmd.Api/Controllers/NewPostController.cs
@@ -1,6 +1,8 @@
 using CQRS.Core.Infrastructure;
 using Microsoft.AspNetCore.Mvc;
 using Post.Cmd.Api.Commands;
+using Post.Cmd.Api.DTOs;
+using Post.Common.DTOs;
 
 namespace Post.Cmd.Api.Controllers;
 
@@ -9,13 +11,45 @@ namespace Post.Cmd.Api.Controllers;
 public class NewPostController : ControllerBase
 {
     private readonly ILogger<NewPostController> _logger;
-    private readonly ICommandDispatcher _cmdDispatcher;
+    private readonly ICommandDispatcher _commandDispatcher;
 
-    public NewPostController(ILogger<NewPostController> logger, ICommandDispatcher cmdDispatcher)
+    public NewPostController(ILogger<NewPostController> logger, ICommandDispatcher commandDispatcher)
     {
         _logger = logger;
-        _cmdDispatcher = cmdDispatcher;
+        _commandDispatcher = commandDispatcher;
     }
-    
-    
+
+    [HttpPost]
+    public async Task<ActionResult> NewPostAsync(NewPostCommand command)
+    {
+        var id = Guid.NewGuid();
+        command.Id = id;
+        try
+        {
+            await _commandDispatcher.SendAsync(command);
+            return StatusCode(StatusCodes.Status201Created, new NewPostResponse()
+            {
+                Message = "Post created",
+            });
+        }
+        catch (InvalidOperationException ex)
+        {
+            _logger.Log(LogLevel.Warning, ex, "Client made bad request");
+            return BadRequest(new BaseResponse()
+            {
+                Message = ex.Message,
+            });
+        }
+        catch (Exception ex)
+        {
+            const string safeErrorMessage = "Error while creating new post";
+            _logger.Log(LogLevel.Error, ex, safeErrorMessage);
+            return StatusCode(StatusCodes.Status500InternalServerError, new NewPostResponse()
+            {
+                Id = id,
+                Message = safeErrorMessage,
+            });
+        }
+        
+    }
 }
