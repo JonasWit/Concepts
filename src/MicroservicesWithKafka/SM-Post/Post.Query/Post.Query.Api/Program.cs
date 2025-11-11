3d78fe2bf85fb9e823dfd7a7d947daa1c583d35e
JWTK <jonaszwitek@gmail.com>
2025-11-11 13:26:26 +0100

    updates

--- src/MicroservicesWithKafka/SM-Post/Post.Query/Post.Query.Api/Program.cs
+++ src/MicroservicesWithKafka/SM-Post/Post.Query/Post.Query.Api/Program.cs
@@ -1,6 +1,10 @@
 using Microsoft.EntityFrameworkCore;
+using Post.Query.Domain.Repositories;
 using Post.Query.Infrastructure.DataAccess;
+using Post.Query.Infrastructure.Handlers;
+using Post.Query.Infrastructure.Repositories;
 using Scalar.AspNetCore;
+using EventHandler = System.EventHandler;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +18,10 @@ builder.Services.AddSingleton(new DatabaseContextFactory(configureDbContext));
 // Create DB
 var dataContext = builder.Services.BuildServiceProvider().GetRequiredService<DatabaseContext>();
 dataContext.Database.EnsureCreated();
+
+builder.Services.AddScoped<IPostRepository, PostRepository>();
+builder.Services.AddScoped<ICommentRepository, CommentRepository>();
+builder.Services.AddScoped<IEventHandler, Post.Query.Infrastructure.Handlers.EventHandler>();
 
 builder.Services.AddOpenApi();
 var app = builder.Build();
