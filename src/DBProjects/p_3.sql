79eea82095d4707b4bc7f2da03fcaea25b9642bd
JWTK <jonaszwitek@gmail.com>
2025-11-23 10:49:23 +0100

    31

--- src/DBProjects/p_3.sql
+++ src/DBProjects/p_3.sql
@@ -2,8 +2,20 @@ USE master
  
 SELECT * FROM sys.database_files
  
-RESTORE FILELISTONLY FROM  DISK = N'AdventureWorks2019.bak' 
+RESTORE FILELISTONLY FROM  DISK = N'AdventureWorks2019.bak' -- just lists the names of the files
  
+USE master;
+GO
+
+ALTER DATABASE AdventureWorks2019
+SET SINGLE_USER
+WITH ROLLBACK IMMEDIATE;
+GO
+
 RESTORE DATABASE AdventureWorks2019 FROM  DISK = N'AdventureWorks2019.bak' 
 WITH MOVE N'AdventureWorks2019' TO N'/var/opt/mssql/data/AdventureWorks2019.mdf',  
      MOVE N'AdventureWorks2019_log' TO N'/var/opt/mssql/data/AdventureWorks2019.ldf'
+
+ALTER DATABASE AdventureWorks2019
+SET MULTI_USER;
+GO
--- src/DBProjects/p_4.sql
+++ src/DBProjects/p_4.sql
@@ -1,8 +1,18 @@
 USE AdventureWorks2019
 GO
 
-SELECT TOP 7 [DepartmentID],
+SELECT  [DepartmentID],
 [Name],
 [GroupName],
 [ModifiedDate] 
 FROM HumanResources.Department
+
+GO
+
+SELECT * FROM HumanResources.Department
+
+DELETE from HumanResources.Department WHERE DepartmentID = 7
+
+SELECT * FROM HumanResources.EmployeeDepartmentHistory 
+
+DELETE FROM HumanResources.EmployeeDepartmentHistory WHERE DepartmentID = 7
